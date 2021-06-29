00dc9309a9f2c914bf2484dd38ee880aadef8532
krolikmvp <dkrolikowski6@gmail.com>
2021-06-30 00:02:04 +0200

    fixed events logic and added logging

--- cogs/warframe.py
+++ cogs/warframe.py
@@ -1,4 +1,4 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import Embed
 import discord
 import logging
@@ -7,6 +7,9 @@ import aiohttp
 import datetime
 import sqlite3
 from utils.load_config import DATABASE_NAME
+from chaos import args
+
+EVENT_REFRESH_TIMEOUT = 600 if not args.debug else 10
 
 
 class Warframe(commands.Cog):
@@ -19,25 +22,29 @@ class Warframe(commands.Cog):
         self.new_events = []
         self.registered_channels = []
         self.notified_channels = []
+        self.event_loop.start()
 
     @commands.Cog.listener()
     async def on_ready(self):
         await self.load_events()
-        while True:
-            if len(self.new_events) > 0:
-                await self.update_channels()
-                await self.get_events()
-                await self.send_events_all_channels()
-                await self.update_events()
-            await asyncio.sleep(10)
 
     @commands.command(name="warframe_events", help="Shows active events in Warframe")
     async def post_events(self, message):
-        print(self.active_events)
         if message.channel.id in self.registered_channels:
             await self.send_events(message.channel)
         else:
             await message.send("This channel is not registered for Warframe info")
+
+    @tasks.loop(seconds=EVENT_REFRESH_TIMEOUT)
+    async def event_loop(self):
+        await self.update_channels()
+        await self.get_events()
+        await self.send_events_all_channels()
+        await self.update_events()
+
+    @event_loop.before_loop
+    async def before_event_loop(self):
+        await self.bot.wait_until_ready()
 
     async def get_alerts(self):
         #TODO
@@ -53,8 +60,10 @@ class Warframe(commands.Cog):
                 if r.status == 200:
                     js = await r.json()
                     for event in js:
+
                         event_id = str(event["id"])
-                        if event["id"] not in self.active_events.keys():
+                        print(f"Event {event_id}")
+                        if event_id not in self.active_events.keys():
                             event_params = dict()
                             event_params["description"] = event["description"]
                             event_params["node"] = event["node"]
@@ -63,23 +72,30 @@ class Warframe(commands.Cog):
                             event_params["expired"] = event["expired"]
                             event_params["expiry"] = event["expiry"]
                             event_params["new"] = False
-                            if event["id"] not in self.loaded_events:
-                                self.new_events.append(event["id"])
+                            if event_id not in self.loaded_events:
+                                self.new_events.append(event_id)
                                 event_params["new"] = True
                             self.active_events[event_id] = event_params
                             self.logging.info(f"New Warframe event appeared: {self.active_events[event_id]}")
+        self.logging.debug(f"Active events: {self.active_events}")
 
     async def send_events_all_channels(self):
-        for channel in self.registered_channels:
-            ch = self.bot.get_channel(channel)
-            await self.send_events(ch)
+        if self.new_events:
+            for channel in self.registered_channels:
+                ch = self.bot.get_channel(channel)
+                await self.send_events(ch)
+            self.logging.info(f"Events sent to all of the channels: {self.new_events}")
+            self.new_events = []
 
     async def send_events(self, channel: discord.TextChannel):
-        for event in self.active_events:
-            await channel.send(embed=self.create_embed(self.active_events[event]))
+        if not self.active_events:
+            await channel.send("There are no active events")
+        else:
+            for event in self.active_events:
+                await channel.send(embed=self.create_embed(self.active_events[event]))
 
     async def update_channels(self):
-
+        self.logging.debug("Updating channels")
         with sqlite3.connect(DATABASE_NAME) as con:
             c = con.cursor()
             querry = c.execute('SELECT channel_id FROM warframe')
@@ -87,6 +103,7 @@ class Warframe(commands.Cog):
                 if row[0] not in self.registered_channels:
                     self.registered_channels.append(row[0])
             c.close()
+        self.logging.debug(f"Registered channels: {self.registered_channels}")
 
     def create_embed(self, event):
         new_event = "[NEW] " if event["new"] else ""
@@ -109,37 +126,45 @@ class Warframe(commands.Cog):
         return embed
 
     async def update_events(self):
-        for event in self.new_events[:]:
-            if event not in self.loaded_events:
-                self.loaded_events.append(event)
+        for new_event in self.new_events[:]:
+            if new_event not in self.loaded_events:
+                self.loaded_events.append(new_event)
 
                 with sqlite3.connect(DATABASE_NAME) as con:
                     c = con.cursor()
-                    c.execute('INSERT INTO "warframe_events" ("event_id") VALUES (?)', (event,))
+                    c.execute('INSERT INTO "warframe_events" ("event_id") VALUES (?)', (new_event,))
                     con.commit()
                     c.close()
-                self.new_events.remove(event)
 
-        for event in self.active_events.keys():
-            self.active_events[event]["new"] = False
-            if event not in self.loaded_events:
-                del self.active_events[event]
-                await self.remove_expired_event(event)
+        for active_event in self.active_events.keys():
+            self.active_events[active_event]["new"] = False
+
+        for loaded_event in self.loaded_events:
+            if not self.active_events.get(loaded_event, False):
+                await self.remove_expired_event(loaded_event)
+            else:
+                if self.active_events[loaded_event]["expired"]:
+                    del self.active_events[loaded_event]
 
     async def load_events(self):
+
         with sqlite3.connect(DATABASE_NAME) as con:
             c = con.cursor()
             querry = c.execute('SELECT event_id FROM warframe_events')
             for row in querry:
                 self.loaded_events.append(row[0])
             c.close()
+        self.logging.debug(f"Loaded events: {self.loaded_events}")
 
     async def remove_expired_event(self, event_id: str):
+
         with sqlite3.connect(DATABASE_NAME) as con:
             c = con.cursor()
-            c.execute(f'DELETE * FROM warframe_events WHERE event_id={event_id}')
+            c.execute(f'DELETE FROM warframe_events WHERE event_id="{event_id}"')
             con.commit()
             c.close()
+
+        self.logging.info(f"Removed expired event: {event_id}")
 
     @staticmethod
     def get_expiration_time(expiration_time):
@@ -159,6 +184,9 @@ class Warframe(commands.Cog):
         delta = expiration_date - time_now
 
         return delta
+
+    def cog_unload(self):
+        self.event_loop.cancel()
 
 
 def setup(bot):
--- constants/timeouts.py
+++
@@ -1,7 +0,0 @@
-from chaos import args
-
-if args.debug:
-    print("Using debug values")
-
-else:
-    pass
